2f815e9cf80488e345786f208b880867fe1c6a1d
MinaWagdy5 <minawagdy387@gmail.com>
2025-03-01 20:47:34 +0200

    added functions except the rgb histogram in ui handler

--- ui_handler.py
+++ ui_handler.py
@@ -7,6 +7,11 @@ import numpy as np
 from add_noise import add_gaussian_noise, add_salt_pepper_noise, add_uniform_noise
 from apply_filter import apply_average_filter, apply_gaussian_filter, apply_median_filter
 from Histogram_Equalization import HistogramEqualization
+from Thresholding import Thresholding
+from RGB2GRAY import RGB2GRAY
+from RGB_Hist import RGB_Hist
+from Freq_filters import Freq_filters
+from Hybrid import Hybrid
 import matplotlib.pyplot as plt
 
 # ui_names:
@@ -153,3 +158,78 @@ class UIHandler:
         self.display_image(self.original_histogram, hist_pixmap)
         self.display_image(self.equalized_histogram, equalized_hist_pixmap)
         self.display_image(self.output_image_view, new_image)
+
+    def convert_to_grayscale(self, image):
+        if len(image.shape) == 3:
+            gray_image = RGB2GRAY.convert_to_grayscale(image)
+        
+        return gray_image
+    
+    def apply_thresholding(self, image, threshold_value, window_size, sensitivity, type):
+        gray_image = self.convert_to_grayscale(image)
+
+        if type == 'global':
+            image = Thresholding.global_thresholding(gray_image, threshold_value)
+
+        elif type == 'local':
+            image = Thresholding.local_thresholding(gray_image, window_size, sensitivity)
+        
+        else:
+            return
+
+        # diplay the thresholded image
+
+    def apply_frequency_filters(self, image, filter_type, D0):
+        image = cv2.resize(image, (256, 256))
+
+        # Convert image to float32 for FFT processing
+        image = np.float32(image)
+
+        # Center the image by multiplying with (-1)^(x+y)
+        rows, cols = image.shape
+        x = np.arange(rows)
+        y = np.arange(cols)
+        X, Y = np.meshgrid(y, x)
+        centered_data = image * ((-1) ** (X + Y))
+
+        # Apply 2D DFT
+        F = Freq_filters.DFT_2D(centered_data)
+
+        # Create a filter
+        H = Freq_filters.create_filter(rows, cols, D0, filter_type)
+
+        # Apply the filter
+        G = Freq_filters.apply_filter(F, H)
+
+        # Apply inverse DFT
+        g = Freq_filters.IDFT_2D(G)
+
+        # Re-center the image
+        recentered_data = g * ((-1) ** (X + Y))
+
+        # Normalize the image data
+        normalized_image = Freq_filters.normalize_image(recentered_data)
+
+        # Convert the normalized image to uint8
+        image = np.array(normalized_image, dtype=np.uint8)
+
+        # display the image
+
+    def hybrid_image(self, image1, image2):
+        # Resize images to the same dimensions
+        image1 = cv2.resize(image1, (256, 256))
+        image2 = cv2.resize(image2, (256, 256))
+
+        image1 = np.float32(image1)
+        image2 = np.float32(image2)
+
+        # Extract frequency components
+        low_frequencies = Hybrid.extract_low_frequencies(image1)
+        high_frequencies = Hybrid.extract_high_frequencies(image2)
+
+        # Combine frequencies
+        hybrid_image = Hybrid.combine_frequencies(low_frequencies, high_frequencies)
+
+        hybrid_image = np.array(hybrid_image, dtype=np.uint8)
+
+        # display hybrid image
